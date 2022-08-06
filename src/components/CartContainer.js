a05da57a1d804b7d8cc0993e16cbb3db920e09f4
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2022-08-06 09:50:17 +0530

    Initial Commit

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CartItem from './CartItem'
 import { useSelector, useDispatch } from 'react-redux'
 import { clearCart } from '../features/cart/cartSlice'
+import { openModal } from '../features/modal/modalSlice'
 
 const CartContainer = () => {
   const { cartItems, total, amount } = useSelector(state => state.cart)
@@ -41,7 +42,7 @@ const CartContainer = () => {
         <button
           className='btn clear-btn'
           onClick={() => {
-            dispatch(clearCart())
+            dispatch(openModal())
           }}
         >
           clear cart
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import cartItems from '../../cartItems'
+import axios from 'axios'
+
+const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-  cartItems,
-  amount: 5,
+  cartItems: [],
+  amount: 0,
   total: 0,
   isLoading: true
 }
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+  return fetch(url)
+    .then(resp =>
+      //console.log(resp.json())
+      resp.json()
+    )
+    .catch(err => console.log(err))
+})
+
+// export const getCartItems = createAsyncThunk(
+//   'cart/getCartItems',
+//   async (name, thunkAPI) => {
+//     try {
+//       // console.log(name);
+//       // console.log(thunkAPI);
+//       // console.log(thunkAPI.getState());
+//       // thunkAPI.dispatch(openModal());
+//       const resp = await axios(url)
+
+//       return resp.data
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue('something went wrong')
+//     }
+//   }
+// )
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -14,6 +43,47 @@ const cartSlice = createSlice({
   reducers: {
     clearCart: state => {
       state.cartItems = []
+    },
+    removeItem: (state, action) => {
+      //console.log(action)
+      const itemId = action.payload
+      state.cartItems = state.cartItems.filter(item => item.id !== itemId)
+    },
+    increase: (state, { payload }) => {
+      //console.log(payload)
+      const cartItem = state.cartItems.find(item => item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1
+      //console.log(cartItem)
+    },
+    decrease: (state, { payload }) => {
+      //console.log(payload)
+      const cartItem = state.cartItems.find(item => item.id === payload.id)
+      if (cartItem.amount > 0) cartItem.amount = cartItem.amount - 1
+      //console.log(cartItem)
+    },
+    calculateTotals: state => {
+      let amount = 0
+      let total = 0
+      state.cartItems.forEach(item => {
+        amount += item.amount
+        total += item.amount * item.price
+      })
+      state.amount = amount
+      state.total = total
+    }
+  },
+  extraReducers: {
+    [getCartItems.pending]: state => {
+      console.log(state)
+      state.isLoading = true
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action)
+      state.isLoading = false
+      state.cartItems = action.payload
+    },
+    [getCartItems.rejected]: state => {
+      state.isLoading = false
     }
   }
 })
@@ -21,4 +91,10 @@ const cartSlice = createSlice({
 //console.log(cartSlice)
 
 export default cartSlice.reducer
-export const { clearCart } = cartSlice.actions
+export const {
+  clearCart,
+  removeItem,
+  increase,
+  decrease,
+  calculateTotals
+} = cartSlice.actions
